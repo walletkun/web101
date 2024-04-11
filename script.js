35e00006c4a53789b540092753d5c047d0f34aaf
Fei Lin <walletkun@gmail.com>
2024-04-11 01:25:51 +0000

    Changed couple things still workign on the slide show but managed to set the dots within the div and made it so it's able to change slides every 3 seconds.

--- script.js
+++ script.js
@@ -104,16 +104,29 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("image");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  let slides = document.querySelectorAll(".image");
+  let dots = document.querySelectorAll(".dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].classList.remove("active");
   }
+
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  dots[slideIndex - 1].classList.add("active");
 }
+
+// Automatically advance slides
+setInterval(function() {
+  plusSlides(1);
+}, 3000); // Change slide every 3 seconds
+
